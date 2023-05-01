3bc0e7ce2f2b26439729d8b3e9e3503acf3bfc7c
jasonc486 <jason.chen@cooper.edu>
2023-05-01 14:31:39 -0400

    got water data

---
+++ M3/graph15.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+file_data = np.loadtxt("data/15psiAirRaw.txt", dtype = float)
+time_interval = 1.0/80.0    #HX711 samples at 80hz
+subArray = file_data[:300]
+subArray = subArray[0:]
+subArray = subArray * 9.81 / 1000
+length = len(subArray)
+t = np.arange(0, time_interval * (length), time_interval)
+
+plt.figure()
+plt.title("Thrust of rocket vs. time")
+plt.xlabel('Time (s)')
+plt.ylabel('Thrust (N)')
+plt.plot(t, subArray)
+plt.show()
